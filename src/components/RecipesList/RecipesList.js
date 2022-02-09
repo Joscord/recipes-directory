e0abf9f67d0e0732fbf2b64581c218aa31391ae2
joscord <josecorderodev@gmail.com>
2022-02-09 18:33:28 -0300

    Modified RecipesList.js and RecipesList.css

--- src/components/RecipesList/RecipesList.js
+++ src/components/RecipesList/RecipesList.js
@@ -1,12 +1,21 @@
-import './RecipesList.css'
+import { Link } from 'react-router-dom';
+import './RecipesList.css';
 
-const RecipesList = ({recipes}) => {
-    return (
-            recipes.map((recipe) => (
-                <div>
-                    <h2>{recipe.title}</h2>
-                </div>
-            ))
-    )
-}
-export default RecipesList
+const RecipesList = ({ recipes }) => {
+	return (
+		<div className='recipes-list'>
+			{recipes.map(({ id, title, cookingTime, method }) => (
+				<div key={id} className='card'>
+					<h2>{title}</h2>
+					<p>{cookingTime} to make.</p>
+					<div>
+						{/* Nótese que creamos un substring del método para luego mostrar más info cuando la persona navegue al detalle de la receta */}
+						{method.substring(0, 100)}
+						<Link to={`/recipes/${id}`}>More info!</Link>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+export default RecipesList;
