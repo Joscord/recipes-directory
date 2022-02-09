1da547f00cba50a00f37df63c83280ef63d0a2fa
joscord <josecorderodev@gmail.com>
2022-02-09 17:18:26 -0300

    Defined routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './pages/Home/Home';
+import Recipe from './pages/Recipe/Recipe';
+import Search from './pages/Search/Search';
+import Create from './pages/Create/Create';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/recipe' component={Recipe}/>
+          <Route path='/search' component={Search}/>
+          <Route path='/create' component={Create}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
