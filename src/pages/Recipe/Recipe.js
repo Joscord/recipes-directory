8118a1078fe9975d6146ac98967e6e8165ae9285
joscord <josecorderodev@gmail.com>
2022-02-09 19:10:21 -0300

    Modified Recipe.js and Recipe.css

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,9 +1,38 @@
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useFetch } from '../../hooks/useFetch';
 import './Recipe.css';
 
 const Recipe = () => {
-  return (
-    <div>Recipe</div>
-  )
-}
+	// Usamos URLSearchParams y useParams para obtener los parámetros de ruta de la URL, específicamente el id
+	const { id } = useParams();
+	// Invocamos a useFetch y obtenemos la data
+	const {
+		data: recipe,
+		isPending,
+		error,
+	} = useFetch(`http://localhost:3000/recipes/${id}`);
 
-export default Recipe
+	return (
+		<div className='recipe'>
+			{error && <p>{error}</p>}
+			{isPending && <p>Loading Recipe...</p>}
+			{recipe && (
+				<>
+					<h2>{recipe.title}</h2>
+					<ul>
+            {
+              recipe.ingredients.map(ing => (
+                // Usamos el ingrediente como llave, más adelante nos aseguraremos que no hay ingredientes repetidos
+                <li key={ing}>{ing}</li>
+              ))
+            }
+          </ul>
+					<p>{recipe.method}</p>
+					<p>{recipe.cookingTime}</p>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Recipe;
