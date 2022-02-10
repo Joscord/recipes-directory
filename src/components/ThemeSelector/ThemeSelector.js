ab99229978a03c3bafcb7459fad1ba620966327d
joscord <josecorderodev@gmail.com>
2022-02-10 18:15:15 -0300

    Modified ThemeContext. Created ThemeSelector.js and ThemeSelector.css

---
+++ src/components/ThemeSelector/ThemeSelector.js
@@ -0,0 +1,24 @@
+import './ThemeSelector';
+// Importamos el custom hook del contexto
+import useTheme from '../../hooks/useTheme';
+
+const ThemeSelector = () => {
+    // Destructuramos la función de cambio de color
+  const { changeColor } = useTheme();
+  // Hacemos un arreglo para los colores
+  const themeColors = ['#58249c', '#249c6b', '#b70233']
+  return (
+    <div className='theme-selector'>
+        <div className='theme-buttons'>
+            {
+                themeColors.map(color => (
+                    // Creamos divs con funciones vinculadas al evento click, así cambiamos los colores
+                    <div key={color} onClick={() => changeColor(color)} style={{background: color}}/>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default ThemeSelector
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -18,14 +18,13 @@ const themeReducer = (state, action) => {
 }
 // Definimos el estado inicial o por defecto
 const defaultState = {
-    color: 'blue'
+    color: '#58249c'
 }
 
 export const ThemeProvider = ({ children }) => {
 	// Cuando invocamos a useReducer pasamos el nombre de nuestra función reductora y como segundo argumento el estado inicial. Este hook retorna dos valores: el estado (el estado inicial que especificamos) y una función de despacho (dispatch function), la que nos permite despachar cambios de estado a nuestra función reductora. La función de despacho recibe un objeto como argumento conocido como una "acción de despacho". En este objeto podemos especificar dos propiedades: type (el tipo de acción, escrito en mayúsculas, por ejemplo CHANGE_COLOR) y el payload, que es data en la que queremos basar nuestro cambio de estado (por ejemplo un nuevo valor para color)
 	const [color, dispatchColor] = useReducer(themeReducer, defaultState);
 
-	// Definimos una función para cambiar el color y también podemos pasar por value y el provider
 	const changeColor = newColor => {
 		dispatchColor({
 			type: 'CHANGE_COLOR',
