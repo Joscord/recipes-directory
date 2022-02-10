490373dfa268fe8825d6e85e8d79f44d87765a3b
joscord <josecorderodev@gmail.com>
2022-02-10 16:54:29 -0300

    Created a ThemeContext

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({children}) => (
+    <ThemeContext.Provider value={{color: blue}}>
+        {children}
+    </ThemeContext.Provider>
+)
