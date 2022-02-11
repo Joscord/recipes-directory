568a6dc1060280c3d34fc8d0b2ca1361a2565bb0
joscord <josecorderodev@gmail.com>
2022-02-11 14:46:13 -0300

    Modified Recipe.js to fetch a document from a firestore collection

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,17 +1,46 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import { useFetch } from '../../hooks/useFetch';
+// importamos el objeto de firestore
+import { projectFirestore } from '../../firebase/config';
 import useTheme from '../../hooks/useTheme';
 import './Recipe.css';
 
+
 const Recipe = () => {
+	// Nuevamente necesitamos 3 piezas de estado
+	const [recipe, setRecipe] = useState(null);
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState(null);
+
 	const { id } = useParams();
-	const {
-		data: recipe,
-		isPending,
-		error,
-	} = useFetch(`http://localhost:3000/recipes/${id}`);
-	// Importamos el tema
 	const { mode } = useTheme()
+	// Usamos useEffect nuevamente
+	useEffect(() => {
+		// Dejamos el estado de pendiente como true como parte del inicio del proceso de fetching. Esta vez lo hice con async/await para mostrar que funciona del mismo modo
+		setIsPending(true);
+		const firestoreFetch = async (id) => {
+			// Hacemos todo dentro de un bloque try/catch
+			try {
+				// Esta vez usamos el método doc, que acepta un id, para tener una referencia al documento en específico de la colección
+				const recipeRef = await projectFirestore.collection('recipes').doc(id);
+				// Ahora obtenemos el snapshot del documento con get(), también es asíncrono
+				const recipeSnapshot = await recipeRef.get();
+				// Evaluamos si el documento existe gracias a la propiedad exists del snapshot
+				if (recipeSnapshot.exists) {
+					// Terminamos el loading
+					setIsPending(false);
+					// Cambiamos el estado pasando la data de la nueva recipe con el método data
+					setRecipe(recipeSnapshot.data());
+				} else {
+					throw new Error('Recipe does not exist')
+				}
+			} catch (err) {
+				setError(err.message);
+				setIsPending(false);
+			}
+		}
+		firestoreFetch(id);
+	}, [])
 
 	return (
 		<div className={`recipe ${mode}`}>
