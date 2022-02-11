26cdcfd87cafeb666d80dd4047fd16cba067509e
joscord <josecorderodev@gmail.com>
2022-02-11 14:16:32 -0300

    Modified Home.js to fetch data from firebase firestore

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,52 @@
 import './Home.css';
-import { useFetch } from '../../hooks/useFetch';
-import RecipesList from '../../components/RecipesList/RecipesList'
+import { useEffect, useState } from 'react';
+import RecipesList from '../../components/RecipesList/RecipesList';
+import { projectFirestore } from '../../firebase/config';
 
 const Home = () => {
-  const url = 'http://localhost:3000/recipes'
-  const { data: recipes, isPending, error } = useFetch(url);  
+	const [recipes, setRecipes] = useState(null);
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState(null);
 
-  return (
-    <div className='home'>
-      {error && <p className='error'>{error}</p>}
-      {isPending && <p className='loading'>Loading Recipes...</p>}
-      {recipes && <RecipesList recipes={recipes}/>}
-    </div>
-  )
-}
+	useEffect(() => {
+		setIsPending(true);
 
-export default Home
+		projectFirestore
+			.collection('recipes')
+			.get()
+			.then(snapshot => {
+				if (snapshot.empty) {
+					setError('No recipes to load');
+					setIsPending(false);
+				} else {
+					let results = [];
+					snapshot.docs.forEach(doc =>
+						results.push({
+							// Creamos un documento para empujar a los resultados. El objeto tiene una prop id que es igual al id que viene en doc y el resto de la data (title, cookingTime...) que viene dada por el método data. Ojo que esto es un objeto así que usamos el spread operator (...) para esparcir las propiedades del objeto en el nuevo objeto.
+							id: doc.id,
+							...doc.data(),
+						})
+					);
+					// Luego simplemente actualizo el estado con results
+					setRecipes(results);
+					// Cambiamos el estado de pendiente
+					setIsPending(false);
+				}
+			})
+			// Añadimos un catch para capturar el error si hay uno
+			.catch(err => {
+				setError(err.message);
+				// Y nuevamente cambiamos el estado de isPending
+				setIsPending(false);
+			});
+	}, []);
+	return (
+		<div className='home'>
+			{error && <p className='error'>{error}</p>}
+			{isPending && <p className='loading'>Loading Recipes...</p>}
+			{recipes && <RecipesList recipes={recipes} />}
+		</div>
+	);
+};
+
+export default Home;
