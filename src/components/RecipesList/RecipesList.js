adab0b9952258689d8e7c2dadb700b4f2b4e81d9
joscord <josecorderodev@gmail.com>
2022-02-11 15:45:50 -0300

    Modified RecipesList.js and RecipesList.css

--- src/components/RecipesList/RecipesList.js
+++ src/components/RecipesList/RecipesList.js
@@ -1,9 +1,19 @@
 import { Link } from 'react-router-dom';
 import './RecipesList.css';
 import useTheme from '../../hooks/useTheme';
+// importamos el objeto de firestore
+import { projectFirestore } from '../../firebase/config';
+// importamos el ícono
+import deleteIcon from '../../assets/icons/delete-icon.svg';
 
 const RecipesList = ({ recipes }) => {
 	const { mode } = useTheme();
+	// Definimos la función para manejar el borrado
+	const handleDelete = (id) => {
+		// Nuevamente usamos el método doc par obtener una referencia del documento. Luego simplemente usamos el método delete() y listo
+		projectFirestore.collection('recipes').doc(id).delete();
+	}
+
 	if (recipes.length === 0) {
 		return <div className='error'>No Recipes to Load</div>;
 	}
@@ -17,6 +27,12 @@ const RecipesList = ({ recipes }) => {
 						{method.substring(0, 100)}
 						<Link to={`/recipes/${id}`}>More info!</Link>
 					</div>
+					<img
+						src={deleteIcon}
+						alt='x-icon'
+						className='delete'
+						onClick={() => handleDelete(id)}
+					/>
 				</div>
 			))}
 		</div>
